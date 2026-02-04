ae178e856c2aad028dcfafd30b7ebf88f7f33d89
sergelorenz <sergelorenz.villasica@gmail.com>
2026-02-04 13:30:23 +0800

    Setup Theme

--- app/src/main/java/com/bvfonaps/stratum/ui/theme/Type.kt
+++ app/src/main/java/com/bvfonaps/stratum/ui/theme/Type.kt
@@ -2,33 +2,53 @@ package com.bvfonaps.stratum.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.bvfonaps.stratum.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+
+val Kantumruy = FontFamily(
+    Font(R.font.kantumruy_regular),
+    Font(R.font.kantumruy_bold, FontWeight.Bold),
+    Font(R.font.kantumruy_light, FontWeight.Light)
+)
+
+val AppTypography = Typography(
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = Kantumruy,
+        fontWeight = FontWeight.Bold,
+        fontSize = 64.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    displayLarge = TextStyle(
+        fontFamily = Kantumruy,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Kantumruy,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Kantumruy,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Kantumruy,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Kantumruy,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Kantumruy,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
     )
-    */
 )
