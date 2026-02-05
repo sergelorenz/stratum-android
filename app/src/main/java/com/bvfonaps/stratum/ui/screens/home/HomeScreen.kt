a68f87e73965529a6beecac59716dffeae931179
sergelorenz <sergelorenz.villasica@gmail.com>
2026-02-06 00:03:49 +0800

    Update Button to have icons

--- app/src/main/java/com/bvfonaps/stratum/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/bvfonaps/stratum/ui/screens/home/HomeScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
@@ -16,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,21 +51,37 @@ fun HomeScreen(
             ) {
                 Button(
                     onClick = { },
-                    modifier = Modifier.fillMaxWidth(0.7f),
+                    modifier = Modifier.fillMaxWidth(0.8f),
                     colors = ButtonDefaults.buttonColors(
                         containerColor = MaterialTheme.colorScheme.tertiary
                     )
                 ) {
-                    Text("Login")
+                    Icon(
+                        painter = painterResource(R.drawable.search_icon),
+                        contentDescription = stringResource(R.string.search_for_server)
+                    )
+                    Spacer(modifier = modifier.width(8.dp))
+                    Text(
+                        text = stringResource(R.string.search_for_server),
+                        style = MaterialTheme.typography.bodySmall
+                    )
                 }
                 OutlinedButton(
                     onClick = { },
-                    modifier = Modifier.fillMaxWidth(0.7f),
+                    modifier = Modifier.fillMaxWidth(0.8f),
                     colors = ButtonDefaults.outlinedButtonColors(
                         contentColor = MaterialTheme.colorScheme.tertiary,
                     )
                 ) {
-                    Text("Login")
+                    Icon(
+                        painter = painterResource(R.drawable.question_mark_icon),
+                        contentDescription = stringResource(R.string.how_stratum_works)
+                    )
+                    Spacer(modifier = modifier.width(8.dp))
+                    Text(
+                        text = stringResource(R.string.how_stratum_works),
+                        style = MaterialTheme.typography.bodySmall
+                    )
                 }
             }
 
