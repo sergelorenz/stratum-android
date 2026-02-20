1c70a873cf4c1d6d8580ee865812d5b580915dfc
sergelorenz <sergelorenz.villasica@gmail.com>
2026-02-21 00:01:53 +0800

    Initialize AuthViewModel

--- app/src/main/java/com/bvfonaps/stratum/ui/viewmodels/factory/AppViewModelProvider.kt
+++ app/src/main/java/com/bvfonaps/stratum/ui/viewmodels/factory/AppViewModelProvider.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.bvfonaps.stratum.StratumApplication
+import com.bvfonaps.stratum.ui.screens.splash.AuthViewModel
 import com.bvfonaps.stratum.ui.screens.splash.DiscoveryViewModel
 
 object AppViewModelProvider {
@@ -13,6 +14,12 @@ object AppViewModelProvider {
             DiscoveryViewModel(
                 authStateHolder = stratumApplication().container.authStateHolder,
                 discoveryRepository = stratumApplication().container.discoveryRepository,
+                authRepository = stratumApplication().container.authRepository
+            )
+        }
+        initializer {
+            AuthViewModel(
+                authStateHolder = stratumApplication().container.authStateHolder,
                 authRepository = stratumApplication().container.authRepository
             )
         }
